fa23da48c27e3433697067366a8106170fe7d445
sylvaingautier <syl.gautier@free.fr>
2023-12-26 11:27:44 +0100

    new file:   Resources/environnements/Dungeon tileset.png 	new file:   Resources/personnages/Link.png 	new file:   main.cpp 	new file:   raylib-5.0_win64_msvc16/CHANGELOG 	new file:   raylib-5.0_win64_msvc16/LICENSE 	new file:   raylib-5.0_win64_msvc16/README.md 	new file:   raylib-5.0_win64_msvc16/include/raylib.h 	new file:   raylib-5.0_win64_msvc16/include/raymath.h 	new file:   raylib-5.0_win64_msvc16/include/rlgl.h 	new file:   raylib-5.0_win64_msvc16/lib/raylib.dll 	new file:   raylib-5.0_win64_msvc16/lib/raylib.lib 	new file:   raylib-5.0_win64_msvc16/lib/raylibdll.lib

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include "Logger.h"
+//#include "GameCore.h"
+
+
+
+
+
+
+void main()
+{
+	
+	// One time log preferences
+	auto logger = Logger::GetInstance();
+
+	auto logLevelStr = "INFO"; // or WARN, DEBUG, INFO
+	auto logLevel = logger->GetLogLevel(logLevelStr);
+
+	auto logFile = "applog.txt";
+
+	auto logOutputStr = "CONSOLE"; // or CONSOLE
+	auto logOutput = logger->GetLogOutput(logOutputStr);
+	logger->SetLogPreferences(logFile, logLevel, logOutput);
+	logger->Log(__FILE__, __LINE__, "********** Debut ***********************", LogLevel::INFO);
+	/*GameCore mGameCore;
+	
+	mGameCore.initGameCore();
+	mGameCore.loopGameCore();
+	mGameCore.endGameCore();*/
+
+	logger->Log(__FILE__, __LINE__, "****************************************", LogLevel::INFO);
+}
